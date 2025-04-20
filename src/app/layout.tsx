6dd1c5ca8152033834ee5f0871d0bd40beb2013e
MadeSatya5 <madesatya505@gmail.com>
2025-04-20 22:20:42 +0700

    feat: add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,27 @@
 import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
 import { Provider } from "@/components/ui/provider";
+import type { Metadata } from "next"; 
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Bunshin Chat",
   description: "User Friendly Chat App",
+  openGraph: {
+    title: "Bunshin Chat",
+    description: "User Friendly Chat App",
+    url: "https://bunshin-chat.vercel.app/login",
+    siteName: "Bunshin Chat",
+    images: [
+      {
+        url: "https://bunshin-chat.vercel.app/bunshin.png", 
+        width: 1200,
+        height: 630,
+        alt: "Bunshin Chat",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
